eee59d6d30ce01dcf7487f17e69029f1144e5ea2
apertorJ <apertor@gmail.com>
2020-02-06 11:54:26 +0300

    hometask5 done

--- hometask5/hometask5.3.py
+++ hometask5/hometask5.3.py
@@ -1,16 +1,28 @@
 class Observable():
 
     def __init__(self, **kwargs):
-        for attr in list(kwargs.items()):
-            self.attr[0] = attr[1]
+        self.__dict__.update(kwargs)
 
 
 class X(Observable):
 
-    def __init__(self, **kwargs):
-        self.obj = Observable(kwargs)
+    # def __init__(self, **kwargs):
+    #     super(X, self).__init__(**kwargs)
+
+    def __str__(self):
+        string = ''
+        for attr in self.__dict__:
+            if attr[:1] != "_":
+                if isinstance(self.__dict__[attr], str):
+                    string += "{} = '{}', ".format(attr, self.__dict__[attr])
+                else:
+                    string += "{} = {}, ".format(attr, self.__dict__[attr])
+        string = "X("+string[:len(string)-2]+")"
+        return string
 
 
 x = X(foo=1, bar=5, _bazz=12, name='Amok', props=('One', 'two'))
-
 print(x)
+print(x.foo)
+print(x.name)
+print(x._bazz)
